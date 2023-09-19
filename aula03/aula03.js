e10efbe320232e4d0a6a2c8a775ccd1c159344f4
paulokauã <kpaulopp@gmail.com>
2023-09-19 14:08:21 -0300

    atualizaçoes aula 03

--- aula03/aula03.js
+++ aula03/aula03.js
@@ -1,1 +1,13 @@
 var nome="vascodagama"
+nome= "Vasvo Da Gama"
+function test() {
+    let nome="vascodagama"
+    if(true){
+        console.log("dentro do if " + nome);
+    };
+    console.log("dentro da funcao " + nome);
+}
+test()
+console.log("fora de tudo " + nome);
+const curs0 = "Javascrpt"
+console.log(curs0);
